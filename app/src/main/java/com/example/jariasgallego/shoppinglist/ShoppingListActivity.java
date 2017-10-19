a3d76d9c86f2219a0456e48ae85dfebb656d1a23
jenni-arias <arias.jenni@gmail.com>
2017-10-19 21:51:03 +0200

    Declarar atributos ArrayList<String> y arrayAdapter<String> Crear lista de ítems y el adaptador de la lista Añadir ítems ficticios Poner el adaptador ListView con setAdapter

--- app/src/main/java/com/example/jariasgallego/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/com/example/jariasgallego/shoppinglist/ShoppingListActivity.java
@@ -2,11 +2,17 @@ package com.example.jariasgallego.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class ShoppingListActivity extends AppCompatActivity {
+
+    private ArrayList<String> itemList;
+    private ArrayAdapter<String> adapter;
 
     private ListView list;
     private Button btn_add;
@@ -20,5 +26,19 @@ public class ShoppingListActivity extends AppCompatActivity {
         list = (ListView) findViewById(R.id.list);
         btn_add = (Button) findViewById(R.id.btn_add);
         edit_item = (EditText) findViewById(R.id.edit_item);
+
+        itemList = new ArrayList<>();
+        itemList.add("Patatas");
+        itemList.add("Papel WC");
+        itemList.add("Helado");
+        itemList.add("Copas Danone");
+
+        adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                itemList
+        );
+
+        list.setAdapter(adapter);
     }
 }
