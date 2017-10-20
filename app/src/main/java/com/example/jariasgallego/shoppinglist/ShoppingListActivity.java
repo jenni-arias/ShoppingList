20397253cb8b5eec8a2329fa260a4849a2153f67
jenni-arias <arias.jenni@gmail.com>
2017-10-20 10:12:42 +0200

    Añadir 'listener' con 'setOnLongClickListener' Crear método 'maybeRemoteItem' Implementar el método anterior:  Crear cuadro de diálogo, crear un recurso para el cuadro de diálogo, borrar item si se indica en el cuadro de diálogo.

--- app/src/main/java/com/example/jariasgallego/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/com/example/jariasgallego/shoppinglist/ShoppingListActivity.java
@@ -1,9 +1,12 @@
 package com.example.jariasgallego.shoppinglist;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -36,7 +39,7 @@ public class ShoppingListActivity extends AppCompatActivity {
         itemList.add("Helado");
         itemList.add("Copas Danone");
 
-        adapter = new ArrayAdapter<String>(
+        adapter = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
                 itemList
@@ -58,6 +61,33 @@ public class ShoppingListActivity extends AppCompatActivity {
         });
 
         list.setAdapter(adapter);
+
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> list, View item, int pos, long id) {
+                maybeRemoveItem(pos);
+                return true;
+            }
+        });
+    }
+
+    private void maybeRemoveItem(final int pos) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.confirm);
+        String fmt = getResources().getString(R.string.confirm_message);
+
+        builder.setMessage(String.format(fmt, itemList.get(pos)));
+
+        builder.setPositiveButton(R.string.remove, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                itemList.remove(pos);
+                adapter.notifyDataSetChanged();
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, null);
+        builder.create().show();
+
     }
 
     private void addItem() {
