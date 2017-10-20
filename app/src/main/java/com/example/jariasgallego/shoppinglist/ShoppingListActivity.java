8019896452352de9f28e4afb881dc9526e90888a
jenni-arias <arias.jenni@gmail.com>
2017-10-20 09:50:57 +0200

    Añadir un 'listener' en el 'botón'. Crear método 'addItem' en el método 'onClick' del 'listener'. Implementar 'addItem' (*Falla el 'if': Si el texto está vacio, añadir este texto a la lista de elementos) Añadir un 'Action' en el teclado para que se añadan los elementos desde el mismo teclado.

--- app/src/main/java/com/example/jariasgallego/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/com/example/jariasgallego/shoppinglist/ShoppingListActivity.java
@@ -2,10 +2,13 @@ package com.example.jariasgallego.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -39,6 +42,31 @@ public class ShoppingListActivity extends AppCompatActivity {
                 itemList
         );
 
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem();              //Método inventado
+            }
+        });
+
+        edit_item.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                addItem();
+                return true;
+            }
+        });
+
         list.setAdapter(adapter);
+    }
+
+    private void addItem() {
+        String item_text = edit_item.getText().toString();
+     //   if(item_text.isEmpty()) {              // .equals("") = .isEmpty()
+            itemList.add(item_text);
+            adapter.notifyDataSetChanged();
+            edit_item.setText("");
+     //   }
+
     }
 }
