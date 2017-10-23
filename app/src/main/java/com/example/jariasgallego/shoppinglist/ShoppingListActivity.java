25c231942ceaeb8e6b12795a4f4fb2a5597859fe
jenni-arias <arias.jenni@gmail.com>
2017-10-23 15:54:26 +0200

    Crear nueva clase 'ShoppingListAdapter' con superclass 'android.widget.ArrayAdapter' Sobrecargar método 'getView'. 'setText' para que el CheckBox obtenga el texto del item obtenido.

--- app/src/main/java/com/example/jariasgallego/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/com/example/jariasgallego/shoppinglist/ShoppingListActivity.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 public class ShoppingListActivity extends AppCompatActivity {
 
     private ArrayList<String> itemList;
-    private ArrayAdapter<String> adapter;
+    private ShoppingListAdapter adapter;
 
     private ListView list;
     private Button btn_add;
@@ -39,9 +39,9 @@ public class ShoppingListActivity extends AppCompatActivity {
         itemList.add("Helado");
         itemList.add("Copas Danone");
 
-        adapter = new ArrayAdapter<>(
+        adapter = new ShoppingListAdapter(
                 this,
-                android.R.layout.simple_list_item_1,
+                R.layout.shopping_item,
                 itemList
         );
 
