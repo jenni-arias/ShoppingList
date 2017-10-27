198b90756e78de47e610b6abdc8fa641011b253e
jenni-arias <arias.jenni@gmail.com>
2017-10-27 09:46:49 +0200

    Crear método readItemList() para leer los datos de un fichero

--- app/src/main/java/com/example/jariasgallego/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/com/example/jariasgallego/shoppinglist/ShoppingListActivity.java
@@ -16,6 +16,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -24,6 +25,7 @@ import java.util.ArrayList;
 public class ShoppingListActivity extends AppCompatActivity {
 
     private static final String FILENAME = "shopping_list.txt";
+    private static final int MAX_BYTES = 8000;
 
     private ArrayList<ShoppingItem> itemList;
     private ShoppingListAdapter adapter;
@@ -50,6 +52,29 @@ public class ShoppingListActivity extends AppCompatActivity {
         }
     }
 
+    private void readItemList(){
+        itemList = new ArrayList<>();
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            byte[] buffer = new byte[MAX_BYTES];
+            int nread = fis.read(buffer);
+            if (nread > 0) {
+                String content = new String(buffer, 0, nread);
+                String[] lines = content.split("\n");
+                for (String line : lines) {  // (int i = 0; i < lines.length; i++)
+                    String[] parts = line.split(";");
+                    itemList.add(new ShoppingItem(parts[0], parts[1].equals("true")));
+                }
+            }
+            fis.close();
+        } catch (FileNotFoundException e) {
+            Log.i("jenn", "readItemList FileNotFountException");
+        } catch (IOException e) {
+            Log.e("jenn", "readItemList: IOException");
+            Toast.makeText(this, R.string.cannot_read, Toast.LENGTH_SHORT).show();
+        }
+    }
+
     @Override
     protected void onStop() {
         super.onStop();
@@ -65,11 +90,7 @@ public class ShoppingListActivity extends AppCompatActivity {
         btn_add = (Button) findViewById(R.id.btn_add);
         edit_item = (EditText) findViewById(R.id.edit_item);
 
-        itemList = new ArrayList<>();
-        itemList.add(new ShoppingItem("Patatas"));
-        itemList.add(new ShoppingItem("Papel WC", true));
-        itemList.add(new ShoppingItem("Helado", true));
-        itemList.add(new ShoppingItem("Copas Danone"));
+        readItemList();
 
         adapter = new ShoppingListAdapter(
                 this,
@@ -80,7 +101,7 @@ public class ShoppingListActivity extends AppCompatActivity {
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addItem();              //Método inventado
+                addItem();
             }
         });
 
@@ -139,6 +160,5 @@ public class ShoppingListActivity extends AppCompatActivity {
             edit_item.setText("");
         }
         list.smoothScrollToPosition(itemList.size()-1);
-
     }
 }
